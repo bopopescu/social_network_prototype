816a8c40dc0ece2ac77c71ce723091137ab226b2
Brandon Odiwuor <brandon.odiwuor@gmail.com>
2019-06-20 22:33:48 +0300

    Update user endpoint

--- api/application.py
+++ api/application.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, jsonify, url_for
-from controllers.users_controller import UsersController
-from helpers.exceptions import *
+from flask import Flask, request, jsonify
+from helpers.exceptions import APIException
+from endpoints.user import UserEndpoint
 
 app = Flask(__name__)
 
@@ -15,44 +15,21 @@ def handle_invalid_usage(error):
 @app.route('/users', methods=['GET', 'POST'])
 def handle_users():
     if request.method == 'GET':
-        return jsonify(UsersController.index())
+        return UserEndpoint.get_users()
 
     elif request.method == 'POST':
-        json_data = request.get_json()
-        if 'username' in json_data and 'email' in json_data and 'password' in json_data:
-            data = {
-                'username': json_data['username'],
-                'email': json_data['email'],
-                'password': json_data['password']
-            }
-        else:
-            raise APIException('User username, email or password must be provided', status_code=400)
-
-        if UsersController.search(data['email']):
-            raise APIException('Email already taken', status_code=400)
-
-        user = UsersController.new(**data)
-        return jsonify(user), 201, {'Location': url_for('handle_user', user_id=user['id'], _external=True)}
+        return UserEndpoint.post(request)
 
 
 @app.route('/users/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
 def handle_user(user_id: int):
     if request.method == 'GET':
-        user = UsersController.show(user_id)
-        if not user:
-            raise APIException('user not found')
-        return jsonify({'data': user})
-
+        return UserEndpoint.get(user_id)
     elif request.method == 'PUT':
-        json_data = request.get_json()
-        data = {
-            'username': json_data['username'],
-            'email': json_data['email']
-        }
-        return jsonify(UsersController.update(user_id, **data))
+        return UserEndpoint.put(user_id, request)
 
     else:
-        return jsonify(UsersController.destroy(user_id))
+        return UserEndpoint.delete(user_id)
 
 
 if __name__ == '__main__':
---
+++ api/endpoints/user.py
@@ -0,0 +1,47 @@
+from flask import jsonify, url_for
+from controllers.users_controller import UsersController
+from helpers.exceptions import APIException
+
+
+class UserEndpoint:
+    @staticmethod
+    def get_users():
+        return jsonify({'data': UsersController.index()})
+
+    @staticmethod
+    def get(user_id: int):
+        user = UsersController.show(user_id)
+        if not user:
+            raise APIException('user not found')
+        return jsonify({'data': user})
+
+    @staticmethod
+    def post(request_obj):
+        json_data = request_obj.get_json()
+        if 'username' in json_data and 'email' in json_data and 'password' in json_data:
+            data = {
+                'username': json_data['username'],
+                'email': json_data['email'],
+                'password': json_data['password']
+            }
+        else:
+            raise APIException('User username, email or password must be provided', status_code=400)
+
+        if UsersController.search(data['email']):
+            raise APIException('Email already taken', status_code=400)
+
+        user = UsersController.new(**data)
+        return jsonify({'data': user}), 201, {'Location': url_for('handle_user', user_id=user['id'], _external=True)}
+
+    @staticmethod
+    def put(user_id, request_obj):
+        json_data = request_obj.get_json()
+        data = {
+            'username': json_data['username'],
+            'email': json_data['email']
+        }
+        return jsonify({'data': UsersController.update(user_id, **data)})
+
+    @staticmethod
+    def delete(user_id: int):
+        return jsonify({'data':  UsersController.destroy(user_id)})
